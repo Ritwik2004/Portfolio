870e9e577d794632a5885a185a983bf9630507e2
Ritwik2004 <ritwikkundu09@gmail.com>
2025-08-15 12:02:12 +0530

    fix: correct import path for TestCaseGenerator image in assets.js

--- Portfolio/src/assets/assets.js
+++ Portfolio/src/assets/assets.js
@@ -3,7 +3,7 @@ import ChatApp from "./ChatApp.png"
 import QuickAI from "./QuickAI.png"
 import Backend from "./backend.png"
 import Frontend from "./frontend.png"
-import TestCaseGenerator from "./import TestCaseGenerator from "
+import TestCaseGenerator from "./TestCaseGenerator.png"
 
 export {
     My_Avatar,
